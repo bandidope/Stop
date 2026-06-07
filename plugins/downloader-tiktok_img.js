98c4f516d7c02f57132f2fec79f81cb5edc49b59
YALLICO OWNER <yallico2024@gmail.com>
2026-06-06 22:04:09 -0500

    Actualizar downloader-tiktok_img.js

--- plugins/downloader-tiktok_img.js
+++ plugins/downloader-tiktok_img.js
@@ -1,47 +1,44 @@
-// Code by Xnuvers007 ft. Jikarinka
-// https://github.com/Xnuvers007/
-// 
-// Mejorado por @BrunoSobrino
-////////////////////////////////////
-
-import axios from 'axios';
-import cheerio from 'cheerio';
+import axios from 'axios'
+import cheerio from 'cheerio'
 let handler = async (m, { conn, text: tiktok, args, command, usedPrefix}) => {
-if (!tiktok) throw '*[❗] Ingresa un enlace de tiktok imagenes, ejemplo: "https://vm.tiktok.com/ZM2cqBRVS/".*';        
+if (!tiktok) throw '🚩 Ingresa un enlace de una publicación de fotos de TikTok junto al comando.'  
 let imagesSent
-if (imagesSent) return;
+if (imagesSent) return
 imagesSent = true    
 try {   
-let tioShadow = await ttimg(tiktok); 
-let result = tioShadow?.data;
+let tioShadow = await ttimg(tiktok)
+let result = tioShadow?.data
 for (let d of result) {
-  await conn.sendMessage(m.chat, {image: {url: d}}, {quoted: m});
- };
+  await conn.sendMessage(m.chat, {image: {url: d}}, {quoted: m})
+ }
 imagesSent = false
 } catch {
     imagesSent = false    
-    throw '*[❗] No se obtuvo respuesta de la página, intente más tarde.*'
+    throw m.react('✖️')
  }
-};
-handler.command = /^(ttimg|tiktokimg)$/i;
+}
+handler.help = ['tiktokimg *<url>*']
+handler.tags = ['img', 'downloader']
+handler.command = /^(ttimg|tiktokimg)$/i
 export default handler;
+handler.register = true 
 
 async function ttimg(link) {
     try {    
-        let url = `https://dlpanda.com/es?url=${link}&token=G7eRpMaa`;    
-        let response = await axios.get(url);
-        const html = response.data;
-        const $ = cheerio.load(html);
+        let url = `https://dlpanda.com/es?url=${link}&token=G7eRpMaa`
+        let response = await axios.get(url)
+        const html = response.data
+        const $ = cheerio.load(html)
         let imgSrc = [];
         $('div.col-md-12 > img').each((index, element) => {
-            imgSrc.push($(element).attr('src'));
-        });
+            imgSrc.push($(element).attr('src'))
+        })
         if (imgSrc.length === 0) {
-            return { data: '*[❗] No se encontraron imágenes en el enlace proporcionado.*' };
+            return { data: '🚩 No se encontraron imágenes en el enlace proporcionado.*' }
         }
-        return { data: imgSrc }; 
+        return { data: imgSrc }
     } catch (error) {
         console.lo (error);
-        return { data: '*[❗] No se obtuvo respuesta de la página, intente más tarde.*'};
-    };
-};
+        return { data: '🚩 No se obtuvo respuesta de la página, intenta más tarde.'}
+    }
+}

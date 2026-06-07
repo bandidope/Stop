e71546cc08b757d0ee87fa7f247ed190facd08b3
YALLICO OWNER <yallico2024@gmail.com>
2026-06-07 12:49:04 -0500

    Crear trend.js

---
+++ plugins/trend.js
@@ -0,0 +1,25 @@
+let handler = async (m, {conn, command}) => {
+  let url = TrendTikTok[Math.floor(Math.random() * TrendTikTok.length)];
+    await conn.sendFile( 
+     m.chat, 
+     url, 
+     "gimage.jpg", 
+     ` 
+ 𝗧𝗥𝗘𝗡𝗗 𝗗𝗘 𝗧𝗜𝗞 𝗧𝗢𝗞`.trim(), m)
+};
+handler.help = ["TrendTikTok"];
+handler.tags = ["internet"];
+handler.command = /^(TrendTikTok)$/i;
+export default handler;
+
+global.TrendTikTok = [
+  "https://qu.ax/Ylk.mp4",
+  "https://qu.ax/gjAe.mp4",
+  "https://qu.ax/LJqP.mp4",
+  "https://qu.ax/sdTz.mp4",
+  "https://qu.ax/sUeO.mp4",
+  "https://qu.ax/EzVe.mp4",
+  "https://qu.ax/uQjN.mp4",
+  "https://qu.ax/FLJU.mp4",
+  "https://qu.ax/GZQe.mp4",
+];

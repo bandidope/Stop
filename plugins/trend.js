b394b7f9f4c17ca17bb0252357c6d3878f312c32
YALLICO OWNER <yallico2024@gmail.com>
2026-06-07 13:12:47 -0500

    Actualizar trend.js

--- plugins/trend.js
+++ plugins/trend.js
@@ -14,4 +14,13 @@ export default handler;
 
 global.chicas = [
   "https://h.uguu.se/CQssvkFW.mp4",
+"https://n.uguu.se/EYXSehZA.mp4",
+"https://o.uguu.se/SRVFXrQp.mp4",
+"https://h.uguu.se/mdlwLrkF.mp4",
+"https://o.uguu.se/itiAFsYL.mp4",
+"https://h.uguu.se/hqAOEQcN.mp4",
+"https://o.uguu.se/FVZCcpqU.mp4",
+"https://n.uguu.se/LeROBZSp.mp4",
+"https://o.uguu.se/VMxmUQsy.mp4",
+
 ];

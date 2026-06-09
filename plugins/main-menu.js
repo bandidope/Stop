5f721324c3d98bc5f14255ccbfad2182b84183be
YALLICO OWNER <yallico2024@gmail.com>
2026-06-09 17:40:41 -0500

    Actualizar main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -31,14 +31,15 @@ const handler = async (m, { conn, usedPrefix }) => {
 
     // Encabezado con Estilo de Caja
 let menu = `${saludar()} ${tag} ✨\n\n`;
-    menu += `︵᷼     ⿻ *Storm* ࣪   ࣭  ࣪ *Wa Bot* ࣭  🐈  ࣪\n\n`;
-    menu += `✿ *Hᴏʟᴀ ${tag}*\n*${saludar}*\n`;
-    menu += `> ꒰꛱ ͜Desarrollado por *Whois Yallico* +51 936 994 155\n`;
-    menu += `𓈒𓏸🌴 \Bot Name:\* Storm Bot 🇦🇱\n`;
-    menu += `*𓈒𓏸🌵 \Activo:\* ${uptime}\n`;
-    menu += `*𓈒𓏸🌵 \Comprar:\* .comprar\n`;
-    menu += `*𓈒𓏸🍃 \Usuarios:\* ${totalreg}`;
-    menu += `> 😸 Si encuentra un comando con errores no dudes en reportarlo con el Creador\n`;
+    menu += `╭╾━━━━╼ 〔 🇦🇱 〕 ╾━━━━╼╮\n`;
+    menu += `┃  🇦🇱 *Storm Bot 🇦🇱*\n`;
+    menu += `┃  👤 *Usuario:* ${tag}\n`;
+    menu += `┃  📈 *Nivel:* ${level}\n`;
+    menu += `┃  ✨ *Exp:* ${exp - min}/${xp}\n`;
+    menu += `┃  💎 *Adquirir:* .comprar\n`;
+    menu += `┃  ⏳ *Activo:* ${uptime}\n`;
+    menu += `┃  👥 *Usuarios:* ${totalUsers}\n`;
+    menu += `╰╾━━━━╼ 〔 🚀 〕 ╾━━━━╼╯\n`;
 
     let categorizedCommands = {};
     Object.values(global.plugins)
